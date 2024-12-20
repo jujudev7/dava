4a53c6f117c79e589aaf11423f3cc4f83239669d
jujudev7 <penna.julien@gmail.com>
2024-12-21 00:02:57 +0100

    heading ok

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,146 @@
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <div className="heading flex h-screen flex-col bg-slate-950 text-white">
+        <h1 className="text-[50vh] font-bold">DAVA</h1>
+        <h2 className="-mt-20 mb-10 ml-6 text-7xl font-thin">
+          Divertissement Ad Vitam Æternam
+        </h2>
+
+        <h3 className="absolute bottom-0 right-0 mb-5 mr-10 flex justify-end font-mono text-xl italic text-slate-400">
+          Dans l&apos;inconscient collectif des Français (hors Corse et DOM-TOM)
+          depuis 2014.
+        </h3>
+      </div>
+      <div className="duo">
+        <h2 className="mb-10 ml-6 text-7xl font-thin">
+          <span className="font-bold">Sacha Béhar</span> et{" "}
+          <span className="font-bold">Augustin Shackelpopoulos</span>, un duo
+          espiègle, volontaire, éveillé, responsable.
+        </h2>
+      </div>
+      <div className="grid-container flex h-screen flex-col">
+        <div className="item group flex-1 bg-[#368524] p-4 text-5xl font-bold text-white transition-[flex] duration-300 hover:flex-[2]">
+          DAVAVAD
+          <div className="video mt-8 hidden group-hover:block">
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/-Fv-GOSezIY?si=Oug2dJQf-joEO43q"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </div>
+        <div className="item group flex-1 bg-[#f00632] p-4 text-5xl font-bold text-white transition-[flex] duration-300 hover:flex-[2]">
+          YOUTUBE
+          <div className="video mt-8 hidden group-hover:block">
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/-Fv-GOSezIY?si=Oug2dJQf-joEO43q"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </div>
+        <div className="item group flex-1 bg-[#ffa500] p-4 text-5xl font-bold text-white transition-[flex] duration-300 hover:flex-[2]">
+          INSTAGRAM
+          <div className="video mt-8 hidden group-hover:block">
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/-Fv-GOSezIY?si=Oug2dJQf-joEO43q"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </div>
+        <div className="item group flex-1 bg-[#000000] p-4 text-5xl font-bold text-white transition-[flex] duration-300 hover:flex-[2]">
+          X / TWITTER
+          <div className="video mt-8 hidden group-hover:block">
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/-Fv-GOSezIY?si=Oug2dJQf-joEO43q"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </div>
+        <div className="item group flex-1 bg-[#266efd] p-4 text-5xl font-bold text-white transition-[flex] duration-300 hover:flex-[2]">
+          FACEBOOK
+          <div className="video mt-8 hidden group-hover:block">
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/-Fv-GOSezIY?si=Oug2dJQf-joEO43q"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </div>
+        <div className="item group flex-1 bg-[#ca4858] p-4 text-5xl font-bold text-white transition-[flex] duration-300 hover:flex-[2]">
+          TIPEE
+          <div className="video mt-8 hidden group-hover:block">
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/-Fv-GOSezIY?si=Oug2dJQf-joEO43q"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </div>
+        <div className="item group flex-1 bg-[#000000] p-4 text-5xl font-bold text-white transition-[flex] duration-300 hover:flex-[2]">
+          TIKTOK
+          <div className="video mt-8 hidden group-hover:block">
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/-Fv-GOSezIY?si=Oug2dJQf-joEO43q"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </div>
+        <div className="item group flex-1 bg-[#8a53fd] p-4 text-5xl font-bold text-white transition-[flex] duration-300 hover:flex-[2]">
+          TWITCH
+          <div className="video mt-8 hidden group-hover:block">
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/-Fv-GOSezIY?si=Oug2dJQf-joEO43q"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
